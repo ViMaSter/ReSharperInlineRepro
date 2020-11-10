566f0c913614aa51c387e324891b5b82833ffcc9
Vincent Mahnke <vincent.mahnke@sunlab.de>
2020-11-10 15:38:06 +0100

    Initial commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Optimization;
+
+namespace ReSharperInlineRepro
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        { }
+    }
+}
